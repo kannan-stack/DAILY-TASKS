95b04837cb9a3170f60d9cde17663e9129b7deb4
kannan-stack <kannanjegan@devaxcel.com>
2024-08-02 18:33:15 +0530

    Update userResolver.ts

--- TYPEORM/src/schema/UserSchema/userResolver.ts
+++ TYPEORM/src/schema/UserSchema/userResolver.ts
@@ -1,15 +1,13 @@
+import userService from "../../service/userService";
 
-export const userResolver ={
-
-        Query:{
-
-            users(){
-                return 
-            },
-            user(){
-                return 
-            }
-
-        }
-
-    }
+export const userResolver = {
+  Query: {
+    users: async (parent: unknown, context: any) => {
+      const newService = new userService();
+      return newService.users();
+    },
+    user(parent: unknown, args: any) {
+      return userService.user(args.user_id);
+    },
+  },
+};
